db044604d64db24c9f60518cadf394af6e19716c
sue445 <sue445@sue445.net>
2018-02-13 08:41:42 +0900

    Revert "Use sue445/heroku-cli"
    This reverts commit 81ad1480d6bd0e2ced8d7520406366b003680255.

--- .circleci/heroku_deploy.sh
+++ .circleci/heroku_deploy.sh
@@ -2,13 +2,4 @@
 
 heroku config:add BUNDLE_WITHOUT="test:development" --app $HEROKU_APP_NAME
 
-# FIXME
-# + git push git@heroku.com:chatwork-mention-task.git ccf21e57d60a3beabab465be4769844860455c39:refs/heads/master
-# fatal: Could not read from remote repository.
-#
-# Please make sure you have the correct access rights
-# and the repository exists.
-# Exited with code 128
-. ./.circleci/retty.sh
-
-retry 3 git push git@heroku.com:$HEROKU_APP_NAME.git $CIRCLE_SHA1:refs/heads/master
+git push git@heroku.com:$HEROKU_APP_NAME.git $CIRCLE_SHA1:refs/heads/master
